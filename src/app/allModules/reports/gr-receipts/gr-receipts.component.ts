4939964aa394156d0e74bc1c9779132562641c27
Pramod D P <80121@DESKTOP-931H0VQ.exalca.corp>
2020-07-22 18:56:27 +0530

    report changes 2

--- src/app/allModules/reports/gr-receipts/gr-receipts.component.ts
+++ src/app/allModules/reports/gr-receipts/gr-receipts.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GRReceiptsComponent implements OnInit {
 
+  isProgressBarVisibile: boolean;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/allModules/reports/report/report.component.ts
+++ src/app/allModules/reports/report/report.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
+  isProgressBarVisibile: boolean;
   constructor() { }
 
   ngOnInit(): void {
